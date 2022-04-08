06e2e388c73fff4911bca0f06a697a2eddd78a6f
Lsawai <63713387+Lsawai@users.noreply.github.com>
2022-04-08 22:07:29 +0530

    Side Bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
 import Sidebar from './Components/Sidebar';
+import Navbar from './NavBar'
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Sidebar/>
     </div>
   );
