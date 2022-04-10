da6f476a7832f159f4050542709a72eb2d18f1e1
Nilesh1502 <79056346+Nilesh1502@users.noreply.github.com>
2022-04-10 14:20:34 +0530

    Chatprogress

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import "./App.css";
 import Sidebar from "./Components/Sidebar";
 import Navbar from "./NavBar";
-import Chatbot from "./Chatbot/Chatbot";
+import Chatbot from "./Components/Chatbot2";
+//import Chatbot from "react-chatbot-kit";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <Switch>
-          <Route exact path="/chat">
-            <Chatbot />
-          </Route>
-        </Switch>
-        <Sidebar />
+        <div className="SidePlusContent">
+          <Sidebar />
+          <Switch>
+            <Route exact path="/chat">
+              <div className="ChatBackground">
+                <Chatbot />
+              </div>
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   );
---
+++ src/Components/Chatbot.js
@@ -0,0 +1,14 @@
+const Chatbot = () => {
+  return (
+    <div className="Chatbot">
+      <h2>This is a chatbot</h2>
+      <iframe
+        height="645"
+        width="525"
+        src="https://bot.dialogflow.com/4f438504-091f-47c5-8f19-ae986e6766ad"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Chatbot;
